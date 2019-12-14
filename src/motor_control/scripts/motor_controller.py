b7e4715c1fca537d79d3efea3a0f29daa5babb63
applehymen <annareasner1995@gmail.com>
2019-12-13 19:56:51 -0600

    Refactored motor_controller.py

--- src/motor_control/scripts/motor_controller.py
+++ src/motor_control/scripts/motor_controller.py
@@ -30,7 +30,7 @@ left_Joy_Index = 1
 # Defines the index in joy.axes that corresponds with the up/down movement of the right joystick
 right_Joy_Index = 4
 
-# 32768 is the max speed of the motor and it has to be divided in half to accomadate the 24 volt power supply protecting the motors from shorting
+# 32768 is the max speed of the motor and it has to be divided in half to accommodate the 24 volt power supply protecting the motors from shorting
 max_speed = 32768 / 2
 
 # Checks if Safety is triggered
@@ -42,8 +42,7 @@ if (joy.msg.buttons[safety_Trigger_Index] is True):
 
 	# uses the value of the joystick position which is always between -1 and 1 to calculate a percentage of max speed
 	left_Wheel_Speed = int(max_speed * left_Joy_Position)
-	right_Wheel_Speed = int(
-		-1 * max_speed * right_Joy_Position)  # -1 is used to reverse the value due to coordinate direction with left wheel
+	right_Wheel_Speed = int(-1 * max_speed * right_Joy_Position)  # -1 is used to reverse the value due to coordinate direction with left wheel
 
 	# uses the function from the manafacture provided code for the motor controller to assign to wheel speed to both wheels with the left or right wheels ID or address
 	left_Wheels_Motor_Controller.DutyM1M2(left_Wheels_ID, left_Wheel_Speed, left_Wheel_Speed)
@@ -73,7 +72,7 @@ def listener():
 	left_Wheels_Motor_Controller.Open()
 	right_Wheels_Motor_Controller.Open()
 	rospy.Subscriber("joy", Joy, assign_Movement)
-	rospy.spin()
+	rospy.spin() #keeps us in the function servicing callbacks
 
 if __name__ == '__main__':
 listener()
